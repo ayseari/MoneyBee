cf8ceaf3a3e85ab94c153cd3e7d23f3bf9079022
Ayşe Arı <ayseari90@gmail.com>
2026-01-14 16:00:40 +0300

    feat: docker support added

---
+++ src/Auth/Tests/MoneyBee.Auth.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace MoneyBee.Auth.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
